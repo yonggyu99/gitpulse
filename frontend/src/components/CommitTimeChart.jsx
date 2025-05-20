1278d20be9ad2f04d501e1195289a437a4068b9a
박용규 <yoyo5411@naver.com>
2025-05-20 20:11:34 +0900

    asdfsdaf

--- frontend/src/components/CommitTimeChart.jsx
+++ frontend/src/components/CommitTimeChart.jsx
@@ -66,7 +66,7 @@ const CommitTimeChart = ({ username }) => {
               className={css.dot}
               style={{ backgroundColor: COLORS[idx % COLORS.length] }}
             />
-            <span>{item.name}asdf</span>
+            <span>{item.name}</span>
           </div>
         ))}
       </div>
